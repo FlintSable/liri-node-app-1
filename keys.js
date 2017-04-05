8f0fc6a34cac0a3ea38eede862423cc34b2fb132
Rutul Patel <rutul-patel@live.com>
2017-04-04 20:42:51 -0700

    Create Keys.js with twitter api key

---
+++ keys.js
@@ -0,0 +1,6 @@
+exports.twitterKeys = {
+    consumer_key: 'N8bEmdYRJkGpJkt2jW69lQEN8',
+    consumer_secret: 'vR5RrhdGzltIJSabjQ0oZEfGClzOmWzdp1BahIwjwkopaFVRxz',
+    access_token_key: '2472398990-nxfgG8YwDOdvKSaPDVuNe3fW4XfAdE8x8r1zcx2',
+    access_token_secret: 'F5l7NiVUHJnKiD1Ba8M9TG18xpdkWAYPCuP29fLrz0Khg'
+};
