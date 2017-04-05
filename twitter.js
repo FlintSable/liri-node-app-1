7b652fabfbe31ce0cf7b70cd3a72b56011f13ee2
Rutul Patel <rutul-patel@live.com>
2017-04-04 20:43:32 -0700

    Update twitter.js with getTweets function

--- twitter.js
+++ twitter.js
@@ -1,18 +1,17 @@
-var t = require('twitter');
+var keys = require('./keys.js');
+var twitter = require('twitter');
 
-var client = new t({
-    consumer_key: 'N8bEmdYRJkGpJkt2jW69lQEN8',
-    consumer_secret: 'vR5RrhdGzltIJSabjQ0oZEfGClzOmWzdp1BahIwjwkopaFVRxz',
-    access_token_key: '2472398990-nxfgG8YwDOdvKSaPDVuNe3fW4XfAdE8x8r1zcx2',
-    access_token_secret: 'F5l7NiVUHJnKiD1Ba8M9TG18xpdkWAYPCuP29fLrz0Khg'
-});
+var client = new twitter(keys.twitterKeys);
 
-//client.get('statuses/user_timeline', { user_id: '_rutulpatel', count: 5 }, function(error, tweets, response) {
-client.get('statuses/user_timeline', { user_id: 'patelarpith', screen_name: 'patelarpith', count: 5 }, function(error, tweets, response) {
-    error && console.log('error', error);
-    tweets.forEach(function(tweet) {
-        console.log('tweet', tweet.text);
+exports.getTweets = function(userName, tweetCount) {
+    tweetCount = tweetCount || 20;
+    console.log("making twitter api call");
+    client.get('statuses/user_timeline', { screen_name: userName, count: tweetCount }, function(error, tweets, response) {
+        error && console.log('error', error);
+        tweets.forEach(function(tweet) {
+            console.log('tweet', tweet.text);
+        });
+        // console.log('tweet', tweets);
+        // console.log('response', response);
     });
-    // console.log('tweet', tweets);
-    // console.log('response', response);
-});
+}
