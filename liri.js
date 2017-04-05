f60b45f127183301d68add436f030bb7c0c68ab8
Rutul Patel <rutul-patel@live.com>
2017-04-04 23:58:18 -0700

    Add do-what-it-says func. to read file w/ commands

--- liri.js
+++ liri.js
@@ -1,46 +1,63 @@
 var twitter = require('./twitter.js');
 var spotify = require('./spotify.js');
 var movie = require('./imdb.js');
+var fs = require('fs');
 
 const MY_TWITTER_NAME = "_rutulpatel";
 
 var inputArr = process.argv.splice(2);
 
 var option = inputArr[0];
-var arg = inputArr.splice(1);
+var arg = inputArr.splice(1).join(" ");
 
-switch (option) {
-    case 'my-tweets':
-        twitter.getTweets(MY_TWITTER_NAME, arg || false);
-        break;
+function liri(option, arg) {
+    switch (option) {
+        case 'my-tweets':
+            twitter.getTweets(MY_TWITTER_NAME, arg || false);
+            break;
 
-    case 'spotify-this-song':
-        if (arg.length > 0) {
-            spotify.getSongsInfo(arg);
-        } else {
-            console.log("USAGE: You need to enter song name. Syntax: spotify-this-song <song-name>");
-        }
-        break;
+        case 'spotify-this-song':
+            if (arg) {
+                spotify.getSongsInfo(arg);
+            } else {
+                console.log("USAGE: You need to enter song name. Syntax: spotify-this-song <song-name>");
+            }
+            break;
 
-    case 'movie-this':
-        if (arg.length > 0) {
-            movie.getMovies(arg);
-        } else {
-            console.log("USAGE: You need to enter movie name. Syntax: movie-this <movie-name>");
-        }
-        break;
+        case 'movie-this':
+            if (arg) {
+                movie.getMovies(arg);
+            } else {
+                console.log("USAGE: You need to enter movie name. Syntax: movie-this <movie-name>");
+            }
+            break;
 
-    case '--help':
-        help();
-        break;
+        case 'do-what-it-says':
+            fs.readFile('random.txt', 'utf8', function(err, data) {
+                if (err) {
+                    console.log(err);
+                } else {
+                    var commandsArr = data.split("\n");
+                    commandsArr.forEach(function(command) {
+                        liri(command.split(",")[0], command.split(",").slice(1).join(" "));
+                    });
 
-    default:
-        if (arg) {
-            console.log("I see what you did, but we dont have that functionality added yet...");
-        } else {
-            console.log("Jeez dude, you dont know how to use me, pass '--help' parameter to see what i can do.");
-        }
-        break;
+                }
+            });
+            break;
+
+        case '--help':
+            help();
+            break;
+
+        default:
+            if (arg) {
+                console.log("I see what you did, but we dont have that functionality added yet...");
+            } else {
+                console.log("Jeez dude, you dont know how to use me, pass '--help' parameter to see what i can do.");
+            }
+            break;
+    }
 }
 
 function help() {
@@ -50,3 +67,7 @@ function help() {
         "spotify-this-song\t <songs name>\n";
     console.log(helpMsg);
 }
+
+
+//start the app
+liri(option, arg);
